e4fdd07c485ec9a37f1d644a4202f95f0d5da70a
Michael Keller <michael.keller@gmail.com>
2013-05-24 16:18:05 -0400

    updated readme with config json info

--- gdoc-to-s3.js
+++ gdoc-to-s3.js
@@ -1,35 +1,25 @@
 var $   = require('jquery'),
-  dsv   = require('dsv'),
-  AWS   = require('aws-sdk'),
-  TweetBot,
-  T;
+    dsv   = require('dsv'),
+    AWS   = require('aws-sdk'),
+    TweetBot,
+    T;
 
-AWS.config.loadFromPath('/path/to/credentials.json');
+var aws_info      = require('./config/aws-info.json')
+    gdoc_info     = require('./config/gdoc-info.json'),
+    tweetbot_info = require('./config/tweetbot-info.json');
+
+AWS.config.loadFromPath(aws_info.credentials);
 var s3 = new AWS.S3();
 
 /* ------------------------ */
 /*    SET UP ACCOUNT INFO   */
 /*      AND FILE SCHEMA     */
 /* ------------------------ */
-var CONFIG = {
-  bucket: '',
-  key: '0Aoev8mClJKw_dFFEUHZLV1UzQmloaHRMdHIzeXVGZFE',
-  output_schema: ['name','color'], // These are the columns to carry over into your file on S3
-  output_path: 'tests/',
-  file_name: 'names.csv',
-  use_twitter_bot: false
-};
 
-if (CONFIG.use_twitter_bot){
+if (tweetbot_info.use_twitter_bot){
   TweetBot = require('twit');
-  T = new TweetBot({
-    consumer_key:         'w',
-    consumer_secret:      'x',
-    access_token:         'y',
-    access_token_secret:  'z'
-  });
-
-}
+  T = new TweetBot( tweetbot_info );
+};
 
 function fetchGDoc(key){
   $.ajax({
@@ -58,7 +48,7 @@ function fetchGDoc(key){
 
 function reportStatus(text){
     console.log(text);
-    if(CONFIG.use_twitter_bot){
+    if(tweetbot_info.use_twitter_bot){
         tweetStatus(text);
     }
 }
@@ -73,7 +63,7 @@ function sanitizeData(json){
   var sanitized_json = [];
   json.forEach(function(row){
     var obj = {};
-    CONFIG.output_schema.forEach(function(col){
+    gdoc_info.output_schema.forEach(function(col){
       obj[col] = row[col];
     });
     sanitized_json.push(obj);
@@ -91,8 +81,8 @@ function uploadToS3(sanitized_csv, timestamp){
 function uploadToS3_backup(sanitized_csv, timestamp){
   var status;
   var data = {
-    Bucket: CONFIG.bucket,
-    Key: CONFIG.output_path + 'backups/' + timestamp + CONFIG.file_name,
+    Bucket: aws_info.bucket,
+    Key: aws_info.output_path + 'backups/' + timestamp + aws_info.file_name,
     Body: sanitized_csv
   };
   s3.client.putObject( data , function (resp) {
@@ -109,8 +99,8 @@ function uploadToS3_backup(sanitized_csv, timestamp){
 function uploadToS3_live(sanitized_csv, timestamp){
   var status;
   var data = {
-    Bucket: CONFIG.bucket,
-    Key: CONFIG.output_path + CONFIG.file_name,
+    Bucket: aws_info.bucket,
+    Key: aws_info.output_path + aws_info.file_name,
     Body: sanitized_csv
   };
   s3.client.putObject( data , function (resp) {
@@ -129,4 +119,4 @@ function getFormattedISOTimeStamp(){
   // and adding an underscore at the end to separate it from the file_name
   return new Date().toISOString().replace(/:/g,'_').replace('Z','') + '_';
 }
-fetchGDoc(CONFIG.key);
+fetchGDoc(gdoc_info.key);
