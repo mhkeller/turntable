d3e04d509e51e640186157d463c81766482880b3
Michael Keller <michael.keller@gmail.com>
2013-05-24 12:10:48 -0400

    added dsv parser, outputs tsv

--- gdoc-to-s3.js
+++ gdoc-to-s3.js
@@ -1,8 +1,9 @@
 var $   = require('jquery'),
+  dsv   = require('dsv'),
   AWS   = require('aws-sdk');
 
-// AWS.config.loadFromPath('/path/to/credentials.json');
-// var s3 = new AWS.S3();
+AWS.config.loadFromPath('/path/to/credentials.json');
+var s3 = new AWS.S3();
 
 /* ------------------------ */
 /*    SET UP ACCOUNT INFO   */
@@ -13,7 +14,8 @@ var CONFIG = {
   key: '0Aoev8mClJKw_dFFEUHZLV1UzQmloaHRMdHIzeXVGZFE',
   output_schema: ['name','color'], // These are the columns to carry over into your csv on S3
   output_path: 'tests/',
-  file_name: 'names.csv'
+  file_name: 'names.csv',
+  delimiter: '\t'
 };
 
 function fetchGDoc(key){
@@ -22,14 +24,12 @@ function fetchGDoc(key){
     success:function(response){
 
       var timestamp      = getFormattedISOTimeStamp();
-      var header_columns = response.split('\n')[0].split(',');
 
       var status        = 'Fetch successful: ' + timestamp;
       reportStatus(status);
 
-      var json          = csvToJSON(response);
+      var json          = dsv.csv.parse(response);
       var sanitized_csv = sanitizeData(json);
-      console.log(sanitized_csv)
 
       uploadToS3(sanitized_csv, timestamp);
 
@@ -47,49 +47,21 @@ function reportStatus(text){
     console.log(text);
 }
 
-function arraysEqual(arr1, arr2) {
-  if(arr1.length !== arr2.length){
-    return false;
-  }
-  for(var i = arr1.length; i--;) {
-    if(arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-  return true;
-}
-
 function sanitizeData(json){
-  var csv = CONFIG.output_schema.join(',') + '\n';
+  var csv = CONFIG.output_schema.join(CONFIG.delimiter) + '\n';
   for (var i = 0; i < json.length; i++){
     var row = [];
     for (var q = 0; q < CONFIG.output_schema.length; q++){
       row.push(json[i][CONFIG.output_schema[q]])
     }
     if (i < json.length - 1){
-      csv += row.join(',') + '\n';
+      csv += row.join(CONFIG.delimiter) + '\n';
     }else{
-      csv += row.join(',');
+      csv += row.join(CONFIG.delimiter);
     }
   }
 
   return csv;
-}
-
-function csvToJSON(response){
-  var json = [];
-  var rows = response.split('\n');
-  var columns = rows[0].split(',');
-  // Skip the first row because it's the header row
-  for (var i = 1; i < rows.length; i++){
-    var obj = {};
-    var vals = rows[i].split(',');
-    for (var q = 0; q < vals.length; q++){
-      obj[columns[q]] = vals[q];
-    }
-    json.push(obj);
-  }
-  return json;
 }
 
 function uploadToS3(sanitized_csv, timestamp){
